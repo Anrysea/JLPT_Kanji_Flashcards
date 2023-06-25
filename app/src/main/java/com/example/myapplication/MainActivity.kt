97e72fcc2c50e8e3aa321f07e6be96d4efd42670
anrys <anrysea@gmail.com>
2023-06-25 21:59:42 +0900

    fix

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,19 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dbHelper = KanjiDatabaseHelper(this)
 
-        fun addKanji(kanji: Kanji) {
-            val db = dbHelper.writableDatabase
-            val values = ContentValues().apply {
-                put("symbol", kanji.symbol)
-                put("on_reading", kanji.onReading)
-                put("meaning_ru", kanji.meaningRu)
-                put("examples", kanji.examples)
-            }
-            db.insert("kanji", null, values)
-            db.close()
-        }
 
         val kanjiData = "小\\ショウ,ちい(さい),こ\\маленький, малый\\小さい(маленький), 小学校(начальная школа), 小説(роман), 小鳥(птичка)"
         val parts = kanjiData.split("\\")
@@ -36,33 +24,7 @@ class MainActivity : AppCompatActivity() {
         val meaningRu = parts[2].trim()
         val examples = parts[3].trim()
 
-        val kanji = Kanji(symbol, onReading, meaningRu, examples)
-        addKanji(kanji)
 
-     fun getAllKanjis(): List<Kanji> {
-         val kanjiList = mutableListOf<Kanji>()
-         val db = dbHelper.readableDatabase
-         val cursor = db.rawQuery("SELECT * FROM kanji", null)
-
-         if (cursor.moveToFirst()) {
-             do {
-                 val symbol = cursor.getString(cursor.getColumnIndexOrThrow("symbol"))
-                 val onReading = cursor.getString(cursor.getColumnIndexOrThrow("on_reading"))
-                 val meaningRu = cursor.getString(cursor.getColumnIndexOrThrow("meaning_ru"))
-                 val examples = cursor.getString(cursor.getColumnIndexOrThrow("examples"))
-                 val kanji = Kanji(symbol, onReading, meaningRu, examples)
-                 kanjiList.add(kanji)
-             } while (cursor.moveToNext())
-         }
-
-         cursor.close()
-         db.close()
-         return kanjiList
-     }
-        val kanjiList = getAllKanjis()
-
-        System.out.println("kanjiList: $kanjiList")
-        System.out.println("1")
 
         // Здесь вы можете устанавливать кандзи и описание динамически
         binding.kanjiText.text = "漢字"
--- app/src/main/java/com/example/myapplication/sampledata/Kanji.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication
-
-data class Kanji(
-    val symbol: String,
-    val onReading: String,
-    val meaningRu: String,
-    val examples: String
-)
